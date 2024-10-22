86b3b56e46785a11b8742c866947745d9a37b709
kaijuxkitty <kaijuxkitty@gmail.com>
2024-10-22 09:09:49 -0500

    unfinished

---
+++ index.js
@@ -0,0 +1,8 @@
+//create array of freelancers with price and occupation
+//
+
+const freelancers= {
+    name: "Alice", price: 30, occupation: "writer",
+    name:"Bob", price: 50, occupation:"teacher",
+};
+ console.log(freelancers)
